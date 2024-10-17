cfe211132ebdbfa4cd1ba2bfc7715608688b17ca
Rachma Fauziah <rachma@Rachmas-MacBook-Air.local>
2024-10-17 21:50:31 +0700

    update html, js

--- index.js
+++ index.js
@@ -36,52 +36,96 @@ function addDate(add){
     return date;
 }
 
-document.querySelector("#today").innerHTML = today();
-// alert(getHari(new Date(),"long"))
-document.querySelector("#today-hari").innerHTML = getHari(new Date(),"long");
-document.querySelector("#today-date").innerHTML = getTanggal(new Date());
+// function dateFormatted(date) {
+//     const options = {
+//         weekday: "long",
+//         year: "numeric",
+//         month: "long",
+//         day: "numeric",
+//     };
+//     const dateFormatted = new Date(date);
+//     return dateFormatted.toLocaleDateString("id-ID", options);
+// }
 
-const nextDay = document.querySelectorAll(".next-day");
-nextDay.forEach((item,index) => {
-    item.innerHTML = getHari(addDate((index+1)),"short");
-});
-const nextDate = document.querySelectorAll(".next-date");
-nextDate.forEach((item,index) => {
-    item.innerHTML = getTanggal(addDate((index+1)));
-});
-const dummyCelcius = ['12º','24º','26º','24º','26º','24º']
-const nextCelcius = document.querySelectorAll(".next-celcius");
-nextCelcius.forEach((item,index) => {
-    item.innerHTML = dummyCelcius[index];
-});
+const url = "https://api.open-meteo.com/v1/forecast?latitude=-6.291458125315&longitude=106.91251756430&current=temperature_2m,relative_humidity_2m,apparent_temperature,is_day,precipitation,rain,showers,weather_code,wind_speed_10m,wind_direction_10m,wind_gusts_10m&daily=weather_code,temperature_2m_max,temperature_2m_min,precipitation_sum,rain_sum,wind_speed_10m_max,wind_gusts_10m_max,wind_direction_10m_dominant"
 
-const modalDay = document.querySelectorAll(".modal-day");
-modalDay.forEach((item,index) => {
-    item.innerHTML = getHari(addDate((index+1)),"short");
-});
-const modalDate = document.querySelectorAll(".modal-date");
-modalDate.forEach((item,index) => {
-    item.innerHTML = getTanggal(addDate((index+1)));
-});
-const modalCelcius = document.querySelectorAll(".modal-celcius");
-modalCelcius.forEach((item,index) => {
-    item.innerHTML = dummyCelcius[index];
-});
+{/* <script src="https://gist.github.com/stellasphere/9490c195ed2b53c707087c8c2db4ec0c.js"></script> */}
 
-function getWeather(){
-    const urlCity = `https://geocoding-api.open-meteo.com/v1/search?name=Jakarta&count=1&language=en&format=json`
+async function getWeather() {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        // alert(data.current)
+        today()
+        document.querySelector("#today").innerHTML = today();
+        document.querySelector("#today-hari").innerHTML = getHari(new Date(data.current.time),"long");
+        document.querySelector("#today-date").innerHTML = getTanggal(new Date(data.current.time));
+        document.querySelector("#today-temperature").innerHTML = data.current.temperature_2m+"º";
+        document.querySelector("#today-desc").innerHTML = `
+        <small>
+        interval: ${data.current.interval}<br/>
+        relative_humidity_2m: ${data.current.relative_humidity_2m}<br/>
+        temperature_2m: ${data.current.temperature_2m}<br/>
+        apparent_temperature: ${data.current.apparent_temperature}<br/>
+        is_day: ${data.current.is_day}<br/>
+        rain: ${data.current.rain}<br/>
+        showers: ${data.current.showers}<br/>
+        weather_code: ${data.current.weather_code}<br/>
+        wind_speed_10m: ${data.current.wind_speed_10m}<br/>
+        wind_direction_10m: ${data.current.wind_direction_10m}<br/>
+        wind_gusts_10m: ${data.current.wind_gusts_10m}<br/>
+        </small>
+        `
+        ;
 
-    fetch(urlCity).then(res => res.json()).then(data => {
-        const latitude = Math.floor(data.results[0].latitude * 100) / 100;
-        const longitude = Math.floor(data.results[0].longitude * 100) / 100;
-        const longLat = [latitude,longitude]
-        const urlWeather = 'https://api.open-meteo.com/v1/forecast?latitude='+longLat[0]+'&longitude='+longLat[1]+'&hourly=temperature_2m,relative_humidity_2m,rain,wind_speed_10m,temperature_80m,is_day'
-        fetch(urlWeather).then(response => response.json()).then(data => {
-            console.log(data)
+        data.daily.time.forEach((el, i) => {
+            if(i>0){
+                document.getElementById("tes").innerHTML += `
+                <a href="#myModal${i}" data-bs-toggle="modal" class="list-group-item list-group-item-action flex-column align-items-start">
+                    <div class="d-flex w-100 justify-content-between">
+                        <p class="card-text next-day">${getHari(new Date(data.daily.time[i]),"short")}</p>
+                        <img class="next-img" src="assets/small-Sun.png">
+                    </div>
+                    <h3 class="card-title next-celcius">${data.daily.temperature_2m_max[i]}º</h3>
+                    <small><small class="next-date">${getTanggal(new Date(data.daily.time[i]))}</small></small>
+                </a>
+                <div class="modal fade" id="myModal${i}" tabindex="-1" role="dialog" aria-hidden="true" data-backdrop="false">
+                    <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
+                        <div class="modal-content glass">
+                            <div class="modal-body">
+                                <div class="d-flex w-100 justify-content-between">
+                                    <img class="modal-img" src="assets/small-Sun.png">
+                                    <div class="text-end align-middle">
+                                        <small class="modal-title modal-day">${getHari(new Date(data.daily.time[i]),"short")}</small><br/>
+                                        <small class="modal-title modal-date">${getTanggal(new Date(data.daily.time[i]))}</small><br/>
+                                        <h2 class="card-title modal-celcius">${data.daily.temperature_2m_max[i]}º</h2>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>`   
+            }
         })
-        .catch(error => {
-            console.error("Error fetching forecast:", error);
-        });
-    })
+
+    } catch (error) {
+        console.log(error);
+    }
 }
-getWeather()
+getWeather();
+
+
+// Code	Description
+// 0	Clear sky
+// 1, 2, 3	Mainly clear, partly cloudy, and overcast
+// 45, 48	Fog and depositing rime fog
+// 51, 53, 55	Drizzle: Light, moderate, and dense intensity
+// 56, 57	Freezing Drizzle: Light and dense intensity
+// 61, 63, 65	Rain: Slight, moderate and heavy intensity
+// 66, 67	Freezing Rain: Light and heavy intensity
+// 71, 73, 75	Snow fall: Slight, moderate, and heavy intensity
+// 77	Snow grains
+// 80, 81, 82	Rain showers: Slight, moderate, and violent
+// 85, 86	Snow showers slight and heavy
+// 95 *	Thunderstorm: Slight or moderate
+// 96, 99 *	Thunderstorm with slight and heavy hail
