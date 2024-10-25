e4c3472dc5eba9ff3f5403976bdd606ccd4fbe9f
Rachma Fauziah <rachma@Rachmas-MacBook-Air.local>
2024-10-25 18:46:46 +0700

    Update styling

--- index.js
+++ index.js
@@ -84,9 +84,9 @@ async function getWeather(latitude, longitude, city) {
         const currentApparentTemperature = parseInt(data.current.apparent_temperature)+"º";
         const currentIsDay               = data.current.is_day+""+data.current_units.is_day;
         const currentPrecipitation       = data.current.precipitation+" "+data.current_units.precipitation;
-        const currentRain                = `${data.current.rain} <small><small>${data.current_units.rain}</small></small>`;
-        const currentShowers             = `${data.current.showers} <small><small>${data.current_units.showers}</small></small>`;
-        const currentSnowfall            = `${data.current.snowfall} <small><small>${data.current_units.snowfall}</small></small>`;
+        const currentRain                = `<h3 class="font-weight-600" id="current-rain">${data.current.rain} <small><small style="font-size:9px;">${data.current_units.rain}</small></small></h3>`;
+        const currentShowers             = `<h3 class="font-weight-600" id="current-rain">${data.current.showers} <small><small style="font-size:9px;">${data.current_units.showers}</small></small></h3>`;
+        const currentSnowfall            = `<h3 class="font-weight-600" id="current-rain">${data.current.snowfall} <small><small style="font-size:9px;">${data.current_units.snowfall}</small></small></h3>`;
         const currentWeatherCode         = data.current.weather_code;
         const currentCloudCover          = data.current.cloud_cover+""+data.current_units.cloud_cover;
         const currentWindSpeed           = data.current.wind_speed_10m+" "+data.current_units.wind_speed_10m;
@@ -94,7 +94,7 @@ async function getWeather(latitude, longitude, city) {
         const currentWindGusts           = data.current.wind_gusts_10m+" "+data.current_units.wind_gusts_10m;
 
         const currentWeather             = wmo[currentWeatherCode].description;
-        const currentDesc                = `Cloud cover ${currentCloudCover}. Relative humidity is ${currentRelativeHumidity}. Precitipation ${currentPrecipitation}. Winds direction ${currentWindDirection} at ${currentWindSpeed} and ${currentWindGusts} gusts.`
+        const currentDesc                = `Cloud cover ${currentCloudCover}. Relative humidity is ${currentRelativeHumidity}. Precitipation ${currentPrecipitation}. Winds direction ${currentWindDirection} at ${currentWindSpeed} and ${currentWindGusts} gusts. Interval ${currentInterval}`
 
         document.querySelector("#current").innerHTML = current() + ' ' + city;
         document.querySelector("#current-weather").innerHTML = currentWeather;
@@ -105,8 +105,9 @@ async function getWeather(latitude, longitude, city) {
 
         document.querySelector("#current-temperature").innerHTML = currentTemperature;
         document.querySelector("#current-apparent-temperature").innerHTML = "Feels like "+ currentApparentTemperature;
-        document.querySelector("#current-interval").innerHTML = "Interval "+ currentInterval;
         document.querySelector("#today-img-div").innerHTML = `<img src="${wmo[data.current.weather_code].image}" width="100%">`;
+        document.querySelector("#today-hari").innerHTML = getHari(new Date(data.current.time),"long");
+        document.querySelector("#today-date").innerHTML = getTanggal(new Date(data.current.time));
         document.querySelector(".hidden").style.display = "block";
 
 
@@ -114,13 +115,13 @@ async function getWeather(latitude, longitude, city) {
         document.getElementById("current-hourly").innerHTML = ``
         for (let i = 0; i < 24; i++) {
             document.getElementById("current-hourly").innerHTML += 
-            `<div class="card glass-effect mb-4" style="min-width:80px;max-width:80px;background:none;margin-right:10px;">
-                <div class="card-body pb-4">
+            `<div class="card glass-effect mt-4" style="min-width:80px;max-width:80px;background:none;margin-right:10px;">
+                <div class="card-body pt-4">
                     <div class="d-flex flex-column position-relative text-center" style="height:100%">
                         <img class="" src="${wmo[data.hourly.weather_code[i]].image}" style="margin:auto; width:40px;height:auto;">
                         <b>${parseInt(data.hourly.temperature_2m[i])}º</b>
-                        <div class="position-absolute start-50 translate-middle glass-effect" style="top:85px;padding:2px 5px;border-radius:7px;">
-                            <small>${data.hourly.time[i].split("T")[1]}</small>
+                        <div class="position-absolute start-50 translate-middle glass-effect" style="top:-20px;padding:2px 5px;border-radius:7px;">
+                            <small><small><b>${data.hourly.time[i].split("T")[1]}</b></small></small>
                         </div>
                     </div>
                 </div>
@@ -133,9 +134,9 @@ async function getWeather(latitude, longitude, city) {
         data.daily.time.forEach((el, i) => {
             if(i>0){
                 document.getElementById("daily").innerHTML += `
-                <div class="d-flex" data-bs-target="#myModal${i}" data-bs-toggle="modal">
-                    <div class="p-2 flex-grow-1" style="text-align:left;">${getHari(new Date(data.daily.time[i]),"short")}</div>
-                    <div class="p-2">${parseInt(data.daily.temperature_2m_max[i])}º</div>
+                <div class="d-flex modal-hover" data-bs-target="#myModal${i}" data-bs-toggle="modal">
+                    <div class="p-2 flex-grow-1" style="text-align:left;"><small>${getHari(new Date(data.daily.time[i]),"short")}</small></div>
+                    <div class="p-2"><b>${parseInt(data.daily.temperature_2m_max[i])}º</b></div>
                     <img class="next-img" src="${wmo[data.daily.weather_code[i]].image}" style="margin:auto; width:50px;height:auto;">
                 </div>
                 <div class="modal fade" id="myModal${i}" tabindex="-1" role="dialog" aria-hidden="true" data-backdrop="false">
@@ -145,9 +146,10 @@ async function getWeather(latitude, longitude, city) {
                                 <div class="d-flex w-100 justify-content-between">
                                     <img class="modal-img" src="${wmo[data.daily.weather_code[i]].image}">
                                     <div class="text-end align-middle">
-                                        <small class="modal-title modal-day">${getHari(new Date(data.daily.time[i]),"short")}</small><br/>
-                                        <small class="modal-title modal-date">${getTanggal(new Date(data.daily.time[i]))}</small><br/>
-                                        <h2 class="card-title modal-celcius">${parseInt(data.daily.temperature_2m_max[i])}º</h2>
+                                        <h5 id="today-hari">${getHari(new Date(data.daily.time[i]),"long")}</h5>
+                                        <p id="today-date">${getTanggal(new Date(data.daily.time[i]))}</p>
+                                        <h2 class="card-title font-size-100 font-weight-700">${parseInt(data.daily.temperature_2m_max[i])}º</h2>
+                                        <p style="">${parseInt(data.daily.temperature_2m_min[i])}º - ${parseInt(data.daily.temperature_2m_max[i])}º</p>
                                     </div>
                                 </div>
                             </div>
